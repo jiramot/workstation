5ef453686a5a12909ca66f9fad47829622db606d
Jiramot Numnam <jiramot@gmail.com>
2015-08-06 04:32:09 +0700

    init

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+et -e
+
+SOURCE_DIR=`dirname "${BASH_SOURCE[0]}"`
+
+# Smoketest environment
+if [[ `which python` ]]; then
+  echo "OK Found python!"
+else
+  echo "Please install python first."
+fi
+
+# Installing most important package
+if [[ `which ansible` ]]; then
+  echo "Ansible is already installed"
+else
+  echo "INFO Installing ansible..."
+  if [[ $OSTYPE == darwin* ]]; then
+    brew install ansible
+  else
+    sudo add-apt-repository -y ppa:rquillo/ansible
+    sudo apt-get update
+    sudo apt-get install -y ansible
+  fi
+fi
+
+# Run ansible
+echo "INFO Execute ansible playbook"
+ansible-playbook -i ${SOURCE_DIR}/hosts.ini -K ${SOURCE_DIR}/playbook.yml $* 
